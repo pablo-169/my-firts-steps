39657ba1ef15bd4581b5393b0c08ef2655162471
Pablo169-Duarte-Tzuc <47620419+Pablo169-Duarte-Tzuc@users.noreply.github.com>
2019-02-13 22:49:21 -0600

    Add files via upload

---
+++ perimeter of square.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main(){
+
+    float a;
+    float b;
+    printf("input a\n");
+    scanf("%f",& a);
+    b=(a*4);
+    
+    printf("the result of the multiplicacion %f",b);
+
+
+    return 0;
+}
